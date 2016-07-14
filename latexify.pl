e7622583c7f7a955f5a236e4585da0aaadfc5925
Ricky Elrod <ricky@elrod.me>
2016-07-14 18:09:28 -0400

    Simplify everything, nuke all except \-commands
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- latexify.pl
+++ latexify.pl
@@ -7,8 +7,8 @@ use File::Basename;
 use vars qw($VERSION %IRSSI);
 $VERSION = '0.001';
 %IRSSI = (
-  authors => 'Patrick Xia',
-  contact => 'patrick.xia@gmail.com',
+  authors => 'Patrick Xia, Ricky Elrod',
+  contact => 'patrick.xia@gmail.com, ricky@elrod.me',
   name => 'latexify',
   description => 'Latexify outgoing messages',
   license => 'BSD',
@@ -34,17 +34,7 @@ Irssi::signal_add_first('send text', 'evt_send_text');
 # - process_starting_modifiers is no longer implemented
 
 my $data_loaded = 0;
-my 
-  (%latex_symbols, 
-   %superscripts, 
-   %subscripts, 
-   %textbb, 
-   %textbf, 
-   %textit,
-   %textcal,
-   %textfrak,
-   %textmono);
-
+my %latex_symbols;
 my $dir = dirname(__FILE__);
 my $pfx = "$dir/data";
 
@@ -60,7 +50,6 @@ sub convert($) {
   return $ss if $ss;
 
   $s = convert_latex_symbols($s);
-  $s = apply_all_modifiers($s);
   return $s;
 }
 
@@ -80,69 +69,8 @@ sub convert_latex_symbols($) {
   return $_;
 }
 
-sub apply_all_modifiers($) {
-  $_ = $_[0];
-  $_ = apply_modifier($_, "^", \%superscripts);
-  $_ = apply_modifier($_, "_", \%subscripts);
-  $_ = apply_modifier($_, "\\bb", \%textbb);
-  $_ = apply_modifier($_, "\\bf", \%textbf);
-  $_ = apply_modifier($_, "\\it", \%textit);
-  $_ = apply_modifier($_, "\\cal", \%textcal);
-  $_ = apply_modifier($_, "\\frak", \%textfrak);
-  $_ = apply_modifier($_, "\\mono", \%textmono);
-  return $_;
-}
-
-# Example: modifier = "^", D = superscripts
-# This will search for the ^ signs and replace the next
-# digit or (digits when {} is used) with its/their uppercase representation.
-sub apply_modifier($$$) {
-  $_ = shift;
-  my $modifier = shift;
-  my %D = %{shift @_};
-
-  s/\Q$modifier\E/\^/;
-
-  # whoo why am I porting this state machine
-  my $newtext = "";
-  my ($mode_normal, $mode_modified, $mode_long) = 0..2;
-  my $mode = $mode_normal;
-  for (split//) {
-    if ($mode == $mode_normal && $_ eq '^') {
-      $mode = $mode_modified;
-      next;
-    } elsif ($mode == $mode_modified) {
-      if ($_ eq '{') {
-        $mode = $mode_long;
-      } elsif ($_ ne ' ') {
-        $newtext .= $D{$_} // $_;
-        $mode = $mode_normal;
-      } 
-      next;
-    } elsif ($mode == $mode_long && $_ eq '}') {
-      $mode = $mode_normal;
-      next;
-    }
-
-    if ($mode == $mode_normal) {
-      $newtext .= $_;
-    } else {
-      $newtext .= $D{$_} // $_;
-    }
-  }
-  return $newtext;
-}
-
 sub load_data() {
   load_dict("$pfx/symbols", \%latex_symbols);
-  load_dict("$pfx/subscripts", \%subscripts);
-  load_dict("$pfx/superscripts", \%superscripts);
-  load_dict("$pfx/textbb", \%textbb);
-  load_dict("$pfx/textbf", \%textbf);
-  load_dict("$pfx/textit", \%textit);
-  load_dict("$pfx/textcal", \%textcal);
-  load_dict("$pfx/textfrak", \%textfrak);
-  load_dict("$pfx/textmono", \%textmono);
 }
 
 sub load_dict($$) {
